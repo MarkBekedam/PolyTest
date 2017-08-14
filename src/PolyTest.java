fa5b73e2525ca2b5298addc71878bba216dd9658
markb <markb@ColorSmith>
2017-08-14 18:46:00 -0400

    Added another polymorphic demonstration of a method that returns any oebject of Economy class.

--- src/PolyTest.java
+++ src/PolyTest.java
@@ -20,7 +20,7 @@ public class PolyTest {
 		ecoCars[2] = geObject;
 		
 		// Polymorphic calls to setExtColor method
-		System.out.println("\n\nA. Polymorphic Array: \n");
+		System.out.println("\n\nA. Polymorphic Array: ");
 		for(int i = 0; i < ecoCars.length; i++) {
 			System.out.print("\n");
 			for(int n = 0; n < ecoColors.length; n++) {
@@ -30,11 +30,19 @@ public class PolyTest {
 		}
 		
 		// Polymorphic argument definition for printCar()
-		System.out.println("\n\n  B. Polymorphic arguments:  \n");
+		System.out.println("\n\n  B. Polymorphic arguments:  ");
 		printCar(ccObject);
 		printCar(deObject);
 		printCar(geObject);
 		
+		// Polymorphic return type for findCar
+		System.out.print("\n\n  C. Polymorphic return values:  \n\n");
+		ccObject.setExtColor("Black");
+		System.out.println("  Find CC0001 color :\t " + findCar("CC0001").getExtColor());
+		geObject.setExtColor("Green");
+		System.out.println("  Find GE0001 color :\t " + findCar("GE0001").getExtColor());
+		deObject.setExtColor("Red");
+		System.out.println("  Find DE0001 color :\t" + findCar("DE0001").getExtColor());
 	}
 	
 	// takes as an argument any object of a class derived from the EConomy class
@@ -42,5 +50,17 @@ public class PolyTest {
 		System.out.print("\n  " 	+ ecoOb.toString() 	+ " 	\t" 
 									+ ecoOb.getUID() 	+ " 	\t"
 									+ ecoOb.getExtColor());
+	}
+	
+	// returns any ojbect of a class derived from the Economy class
+	public static Economy findCar(String carUID) {
+		Economy tempCar = new Economy();
+		for(int i = 0; i < ecoCars.length; i++) {
+			if(ecoCars[i].getUID().equalsIgnoreCase(carUID)) {
+				tempCar = ecoCars[i];
+				break;
+			}
+		}
+		return tempCar;
 	}
 }
