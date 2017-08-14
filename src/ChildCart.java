f23754e44b6946da55bcaf9ad54faf96c02de36d
markb <markb@ColorSmith>
2017-08-14 16:59:27 -0400

    Added ChildCart subclass for running polymorphic demonstrations.

---
+++ src/ChildCart.java
@@ -0,0 +1,35 @@
+
+
+public class ChildCart extends Economy {
+	
+	private String cCartColor;
+	
+	public ChildCart() {
+		super();
+	}
+	
+	public ChildCart(String ccid) {
+		super(ccid);
+	}
+	
+	public void setExtColor(String ec) {
+		if(ec.equalsIgnoreCase("blue"))
+			cCartColor = "sky";
+		else if(ec.equalsIgnoreCase("red"))
+			cCartColor = "Ribbon";
+		else if(ec.equalsIgnoreCase("green"))
+			cCartColor = "Spring";
+		else if(ec.equalsIgnoreCase("yellow"))
+			cCartColor = "Home";
+		else if(ec.equalsIgnoreCase("white"))
+			cCartColor = "Day";
+		else if(ec.equalsIgnoreCase("blacl"))
+			cCartColor = "Night";
+		else
+			cCartColor = "Day";
+	}
+	
+	public String getExtColor() {
+		return cCartColor;
+	}
+}
