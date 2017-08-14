779532947b24748687841e6cff393140b7233cf0
markb <markb@ColorSmith>
2017-08-14 15:54:24 -0400

    Added Economy car subclass.

---
+++ src/Economy.java
@@ -0,0 +1,21 @@
+
+public class Economy extends Car {
+
+	private boolean rearDoorStatus;
+	
+	public Economy() {
+		super();
+	}
+	
+	public Economy(String uid) {
+		super(uid);
+	}
+	
+	public void setRearDoorStatus(boolean rds) {
+		rearDoorStatus = rds;
+	}
+	
+	public boolean getRearDoorStatus() {
+		return rearDoorStatus;
+	}
+}
