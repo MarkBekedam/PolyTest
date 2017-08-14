8f514655078da9f98585c1fecf67c9a101c18d90
markb <markb@ColorSmith>
2017-08-14 15:50:17 -0400

    Added FourDoor car subclass.

---
+++ src/FourDoor.java
@@ -0,0 +1,29 @@
+public class FourDoor extends Car {
+
+	private boolean leftBackPassengerDoorStatus;
+	private boolean rightBackPassengerDoorStatus;
+	
+	public FourDoor() {
+		super();
+	}
+	
+	public FourDoor(String uid) {
+		super(uid);
+	}
+	
+	public void setLeftBackPassengerDoorStatus(boolean lbpds) {
+		leftBackPassengerDoorStatus = lbpds;
+	}
+	
+	public boolean getLeftBackPassengerDoorStatus() {
+		return leftBackPassengerDoorStatus;
+	}
+	
+	public void setRightBackPassengerDoorStatus(boolean rbpds) {
+		rightBackPassengerDoorStatus = rbpds;
+	}
+	
+	public boolean getRightBackPassengerDoorStatus() {
+		return rightBackPassengerDoorStatus;
+	}
+}
