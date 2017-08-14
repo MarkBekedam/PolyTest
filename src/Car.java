83d0ccb7bb97dd8457fdd6ce6e3fbc62138a9f1f
markb <markb@ColorSmith>
2017-08-14 15:27:24 -0400

    Initial checkin of polytest.

---
+++ src/Car.java
@@ -0,0 +1,93 @@
+
+public class Car {
+
+	private String exteriorColor;
+	private float levelOfFuel;
+	private String carType;
+	private boolean hoodStatus;
+	private float kphSpeed;
+	private int wipersSpeed;
+	private boolean driverDoorStatus;
+	private boolean frontPassengerDoorStatus;
+	private String uniqueID;
+	
+	Car() {
+		uniqueID = "0000AA";
+	}
+	
+	Car(String uid) {
+		uniqueID = uid;
+	}
+	
+	public String getUID() {
+		return uniqueID;
+	}
+	
+	public void setUID(String uid) {
+		uniqueID = uid;
+	}
+	
+	public void setExtColor(String ec) {
+		exteriorColor = ec;
+	}
+	
+	public String getExtColor() {
+		return exteriorColor;
+	}
+	
+	public void setLevelOfFuel(float fl) {
+		levelOfFuel = fl;
+	}
+	
+	public float getLevelOfFuel() {
+		return levelOfFuel;
+	}
+	
+	public void setCarType(String ct) {
+		carType = ct;
+	}
+	
+	public String getCarType() {
+		return carType;
+	}
+	
+	public void setKphSpeed(float ks) {
+		kphSpeed = ks;
+	}
+	
+	public float getKphSpeed() {
+		return kphSpeed;
+	}
+	
+	public void setWipersSpeed(int ws) {
+		wipersSpeed = ws;
+	}
+	
+	public int getWipersSpeed() {
+		return wipersSpeed;
+	}
+	
+	public void setDriverDoorStatus(boolean ds) {
+		driverDoorStatus = ds;
+	}
+	
+	public boolean getDriverDoorStatus() {
+		return driverDoorStatus;
+	}
+	
+	public void setFrontPassengerDoorStatus(boolean fpds) {
+		frontPassengerDoorStatus = fpds;
+	}
+	
+	public boolean getFrontPassengerDoorStatus() {
+		return frontPassengerDoorStatus;
+	}
+	
+	public void setHoodStatus(boolean hs) {
+		hoodStatus = hs;
+	}
+	
+	public boolean getHoodStatus() {
+		return hoodStatus;
+	}
+}
