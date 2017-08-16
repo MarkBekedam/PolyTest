fb6d33ddbf0b7d964b4bc71901fb503f7f5f3954
markb <markb@DESKTOP-7NSO2UC>
2017-08-16 17:03:38 -0400

    Created reference variable to Economy type and used it to handle various type casting of car objects that stem from the abstract economy type.

--- src/PolyTest.java
+++ src/PolyTest.java
@@ -3,30 +3,40 @@
  *  other than the obvious of type casting one object to another.
  */
 
+import java.util.Vector;
+
 public class PolyTest {
 
-	static Economy[] ecoCars = new Economy[3];
+	static Vector<Economy> ecoCars = new Vector<Economy>();
 	static String[] ecoColors = {"Blue", "Red", "Green", 
 								"Yellow", "White", "Black"};
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		ChildCart ccObject = new ChildCart("CC0001");
 		DieselElectric deObject = new DieselElectric("DE0001");
 		GasElectric geObject = new GasElectric("GE0001");
 		
-		ecoCars[0] = ccObject;
-		ecoCars[1] = deObject;
-		ecoCars[2] = geObject;
+		// assign colors to the three concrete objects
+		ccObject.setExtColor(ecoColors[0]);
+		deObject.setExtColor(ecoColors[1]);
+		geObject.setExtColor(ecoColors[2]);
 		
-		// Polymorphic calls to setExtColor method
-		System.out.println("\n\nA. Polymorphic Array: ");
-		for(int i = 0; i < ecoCars.length; i++) {
-			System.out.print("\n");
-			for(int n = 0; n < ecoColors.length; n++) {
-				ecoCars[i].setExtColor(ecoColors[n]);
-				System.out.println(ecoCars[i].getExtColor() + "  ");
-			}
+		// make a pointer to a reference variable of economy type
+		Economy ref;
+		
+		// add the objects to the vector container
+		ecoCars.add(ccObject);
+		ecoCars.add(deObject);
+		ecoCars.add(geObject);
+		
+		// Polymorphic calls to the getExtColor method
+		System.out.println("\n\n  A. Polymorphic Presentation:  ");
+		
+		for (int i = 0; i < ecoCars.size(); i++) {
+			// cast the objects to the abstract super class type and store them in a reference.
+			ref = (Economy)ecoCars.get(i);
+			System.out.println("  " + ref.getExtColor());
 		}
 		
 		// Polymorphic argument definition for printCar()
@@ -36,13 +46,13 @@ public class PolyTest {
 		printCar(geObject);
 		
 		// Polymorphic return type for findCar
-		System.out.print("\n\n  C. Polymorphic return values:  \n\n");
+		System.out.print("\n\n  C. Polymorphic return values:  \n");
 		ccObject.setExtColor("Black");
 		System.out.println("  Find CC0001 color :\t " + findCar("CC0001").getExtColor());
 		geObject.setExtColor("Green");
 		System.out.println("  Find GE0001 color :\t " + findCar("GE0001").getExtColor());
 		deObject.setExtColor("Red");
-		System.out.println("  Find DE0001 color :\t" + findCar("DE0001").getExtColor());
+		System.out.println("  Find DE0001 color :\t " + findCar("DE0001").getExtColor());
 	}
 	
 	// takes as an argument any object of a class derived from the EConomy class
@@ -54,10 +64,14 @@ public class PolyTest {
 	
 	// returns any ojbect of a class derived from the Economy class
 	public static Economy findCar(String carUID) {
-		Economy tempCar = new Economy();
-		for(int i = 0; i < ecoCars.length; i++) {
-			if(ecoCars[i].getUID().equalsIgnoreCase(carUID)) {
-				tempCar = ecoCars[i];
+		
+		// create a reference and cast it
+		Economy tempCar = null;
+		for(int i = 0; i < ecoCars.size(); i++) {
+			// cast it after it is retrieved from the container and put it in the vector
+			tempCar = (Economy)ecoCars.get(i);
+			if(carUID.equalsIgnoreCase(tempCar.getUID())) {
+				tempCar = (Economy)ecoCars.get(i);
 				break;
 			}
 		}
